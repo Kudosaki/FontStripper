9aba1797155df8d00c8655a3d270c2b8d99b8831
Kudosaki <154644607+Kudosaki@users.noreply.github.com>
2026-06-24 16:09:03 +0700

    Update FontStripperPlugin.java

--- src/main/java/com/yourname/fontstripper/FontStripperPlugin.java
+++ src/main/java/com/yourname/fontstripper/FontStripperPlugin.java
@@ -5,7 +5,6 @@ import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FontStripperPlugin extends JavaPlugin {
-    
     @Override
     public void onLoad() {
         PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this));
@@ -15,13 +14,11 @@ public class FontStripperPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         PacketEvents.getAPI().init();
-        
         getServer().getPluginManager().registerEvents(new InventoryStateHandler(this), this);
         ItemPacketEventsInterceptor.register(this);
-        
-        getLogger().info("FontStripper successfully armed for 1.21.1!");
+        getLogger().info("FontStripper enabled!");
     }
-    
+
     @Override
     public void onDisable() {
         PacketEvents.getAPI().terminate();
