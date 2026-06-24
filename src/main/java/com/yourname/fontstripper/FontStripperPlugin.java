5709d455d307bd059f6f0ccdd0c307172c7be6e1
Kudosaki <154644607+Kudosaki@users.noreply.github.com>
2026-06-24 16:59:25 +0700

    Update FontStripperPlugin.java

--- src/main/java/com/yourname/fontstripper/FontStripperPlugin.java
+++ src/main/java/com/yourname/fontstripper/FontStripperPlugin.java
@@ -16,7 +16,7 @@ public class FontStripperPlugin extends JavaPlugin {
         PacketEvents.getAPI().init();
         getServer().getPluginManager().registerEvents(new InventoryStateHandler(this), this);
         ItemPacketEventsInterceptor.register(this);
-        getLogger().info("[FontStripper] Plugin enabled and compiled successfully.");
+        getLogger().info("[FontStripper] Debug-Enabled version loaded.");
     }
 
     @Override
