f6644bb830f9f0f71e69e54390b0328c112f5b1b
Kudosaki <154644607+Kudosaki@users.noreply.github.com>
2026-06-24 17:10:17 +0700

    Update InventoryStateHandler.java

--- src/main/java/com/yourname/fontstripper/InventoryStateHandler.java
+++ src/main/java/com/yourname/fontstripper/InventoryStateHandler.java
@@ -1,36 +1,21 @@
 package com.yourname.fontstripper;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 public class InventoryStateHandler implements Listener {
     public static final Set<UUID> openInventories = new HashSet<>();
-    private final JavaPlugin plugin;
-
-    public InventoryStateHandler(JavaPlugin plugin) {
-        this.plugin = plugin;
-    }
-
-    @EventHandler
-    public void onInventoryOpen(InventoryOpenEvent event) {
-        if (event.getPlayer() instanceof Player) {
-            openInventories.add(event.getPlayer().getUniqueId());
-            plugin.getLogger().info("[FontStripper Debug] Inventory Open detected for: " + event.getPlayer().getName());
-        }
-    }
 
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
         if (event.getPlayer() instanceof Player) {
             openInventories.remove(event.getPlayer().getUniqueId());
-            plugin.getLogger().info("[FontStripper Debug] Inventory Close detected for: " + event.getPlayer().getName());
+             System.out.println("[Debug] Inventory Closed: " + event.getPlayer().getName());
         }
     }
 }
