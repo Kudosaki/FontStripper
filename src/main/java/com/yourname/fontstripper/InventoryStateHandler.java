336fe3dc0825a70701dfeacfd9db4c27fb1bbdc4
Kudosaki <154644607+Kudosaki@users.noreply.github.com>
2026-06-24 17:17:27 +0700

    Update InventoryStateHandler.java

--- src/main/java/com/yourname/fontstripper/InventoryStateHandler.java
+++ src/main/java/com/yourname/fontstripper/InventoryStateHandler.java
@@ -4,18 +4,30 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 public class InventoryStateHandler implements Listener {
+
+    // Players whose inventories are currently open
     public static final Set<UUID> openInventories = new HashSet<>();
 
     @EventHandler
+    public void onInventoryOpen(InventoryOpenEvent event) {
+        if (event.getPlayer() instanceof Player player) {
+            openInventories.add(player.getUniqueId());
+            System.out.println("[FontStripper] Inventory Opened: " + player.getName());
+        }
+    }
+
+    @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
-        if (event.getPlayer() instanceof Player) {
-            openInventories.remove(event.getPlayer().getUniqueId());
-             System.out.println("[Debug] Inventory Closed: " + event.getPlayer().getName());
+        if (event.getPlayer() instanceof Player player) {
+            openInventories.remove(player.getUniqueId());
+            System.out.println("[FontStripper] Inventory Closed: " + player.getName());
         }
     }
 }
